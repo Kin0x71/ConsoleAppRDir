b5c27cdf0e44baf33925de35975b1d62f75ba934
Kin0x71 <kin0x71@gmail.com>
2022-04-13 02:34:09 +1000

    add binaries

--- run.bat
+++ run.bat
@@ -1,3 +1,2 @@
-cd C:\Users\Kin\source\repos\ConsoleAppRDir\ConsoleAppRDir\bin\x64\Release\net6.0
-ConsoleAppRDir.exe -p "C:\Users\Kin\source\repos\ConsoleAppRDir\test_folder" -h -q
+ConsoleAppRDir.exe -p "test_folder" -h -q
 pause
